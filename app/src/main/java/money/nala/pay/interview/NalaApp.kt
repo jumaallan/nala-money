791fd3b042f7e7edc4e29b3801dbc20758454598
Juma Allan <allanjuma@gmail.com>
2020-04-20 20:44:19 +0300

    Setting up Timber and KOIN

--- app/src/main/java/money/nala/pay/interview/NalaApp.kt
+++ app/src/main/java/money/nala/pay/interview/NalaApp.kt
@@ -2,6 +2,14 @@ package money.nala.pay.interview
 
 import android.app.Application
 import android.content.Context
+import androidx.annotation.Nullable
+import money.nala.pay.interview.di.injectFeature
+import org.jetbrains.annotations.NotNull
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.error.KoinAppAlreadyStartedException
+import timber.log.Timber
 
 class NalaApp : Application() {
 
@@ -9,9 +17,35 @@ class NalaApp : Application() {
         super.onCreate()
 
         appContext = applicationContext
+
+        initTimber()
+        initKOIN()
+        injectFeature()
     }
 
     companion object {
         lateinit var appContext: Context
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                @Nullable
+                override fun createStackElementTag(@NotNull element: StackTraceElement): String? {
+                    return super.createStackElementTag(element) + ":" + element.lineNumber
+                }
+            })
+        }
+    }
+
+    private fun initKOIN() {
+        try {
+            startKoin {
+                androidLogger()
+                androidContext(applicationContext)
+            }
+        } catch (error: KoinAppAlreadyStartedException) {
+            Timber.e(error.localizedMessage)
+        }
     }
 }
